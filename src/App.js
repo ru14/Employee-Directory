d055499dc928dbbb6700a43bbdd97967587f215b
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-17 16:10:24 -0700

    base line code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import HelloReact from "./components/React";
+import useFetchUsers from "./useFetchUsers";
 
 function App() {
-  return <HelloReact />;
+  const {users, loading, error} = useFetchUsers()
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error...</h1>}
+      <h1>{users.length}</h1>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/useFetchUsers.js
@@ -0,0 +1,30 @@
+
+import { useReducer, useEffect } from "react"
+
+const ACTIONS ={
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
+
+
+function reducer(state, action){
+switch(action.type){
+    case ACTIONS.MAKE_REQUEST:
+        return{loading: true,users: []}
+        case ACTIONS.GET_DATA:
+            return{...state, loading: false, users:action.payload.users}
+        case ACTIONS.ERROR:
+            return{...state, loading: false, users:action.payload.error, users: []
+            }
+}
+
+export default function useFetchUsers(params, page){
+    const [state, dispatch]= useReducer(reducer,{users: [], loading:true})
+    
+return{
+    users: [],
+    loading: false,
+    error: false
+}
+}
