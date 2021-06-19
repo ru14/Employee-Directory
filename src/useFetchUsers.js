81d047706dfcb7a91f781200b73842746a2dda7e
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-19 11:29:59 -0700

    error fixed

--- src/useFetchUsers.js
+++ src/useFetchUsers.js
@@ -1,30 +1,34 @@
 
 import { useReducer, useEffect } from "react"
 
-const ACTIONS ={
+const ACTIONS = {
     MAKE_REQUEST: 'make-request',
     GET_DATA: 'get-data',
     ERROR: 'error'
 }
 
-
-function reducer(state, action){
-switch(action.type){
-    case ACTIONS.MAKE_REQUEST:
-        return{loading: true,users: []}
-        case ACTIONS.GET_DATA:
-            return{...state, loading: false, users:action.payload.users}
-        case ACTIONS.ERROR:
-            return{...state, loading: false, users:action.payload.error, users: []
-            }
+//action is varable and state is current state
+function reducer(state, action) {
+    switch (action.type) {
+        case
+            ACTIONS.MAKE_REQUEST:
+            return { loading: true, users: [] }
+        case
+            ACTIONS.GET_DATA:
+            return { ...state, loading: false, users: action.payload.users }
+        case
+            ACTIONS.ERROR:
+            return {...state, loading: false, users: action.payload.error, users: [] }
+default: 
+return state
+    }
 }
+export default function useFetchUsers(params, page) {
+    const [state, dispatch] = useReducer(reducer, { users: [], loading: true })
 
-export default function useFetchUsers(params, page){
-    const [state, dispatch]= useReducer(reducer,{users: [], loading:true})
-    
-return{
-    users: [],
-    loading: false,
-    error: false
+    return {
+        users: [],
+        loading: false,
+        error: false
+    }
 }
-}}
