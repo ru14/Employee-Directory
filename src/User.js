049e8f8814aa4b096d8c944f415d534be3060bab
ru14 <74753345+ru14@users.noreply.github.com>
2021-06-22 13:32:22 -0700

    trying fixing error

--- src/User.js
+++ src/User.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
-export default function User({user}){
-    return(
+export default function User({ user }) {
+    return (
         <div>
-<p>{user.name.first} {user.name.last}</p>
+            <p>user.name.first</p>
         </div>
+        // <Card>
+        //     <Card.Body>
+        //         <div>
+        //             <Card.Name>
+        //                 {user.name.first} {user.name.last}
+        //             </Card.Name>
+        //         </div>
+        //         <div>
+        //             <Card.Phone>
+        //                 {user.phone} {user.phone}
+        //             </Card.Phone>
+        //         </div>
+        //         <div>
+        //             <Card.Email>
+        //                 {user.email} {user.email}
+        //             </Card.Email>
+        //         </div>
+        //         <div>
+        //             <Card.DOB>
+        //                 {user.dob} {user.dob}
+        //             </Card.DOB>
+        //         </div>
+        //     </Card.Body>
+        // </Card>
     )
 }
